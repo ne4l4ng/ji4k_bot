03aa5c781ced9fb78f65e42b608e4f78c3209471
Sh4d3 <cyph3rd@gmail.com>
2018-07-09 06:58:06 +0800

    including makan_places table

--- dbhelper.py
+++ dbhelper.py
@@ -9,9 +9,14 @@ class DBHelper:
         self.conn = sqlite3.connect(dbname)
 
     def setup(self):
+        makan_tblstmt = "CREATE TABLE IF NOT EXISTS makan_places (name text, votes int)"
         tblstmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
         itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)"
         ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+        insert_makan_stmt = "INSERT INTO makan_places (name, votes) VALUES ('Cafeteria', 0),('Opposite', 0)," \
+                            "('Fish Soup', 0),('Collins', 0)"
+        self.conn.execute(makan_tblstmt)
+        self.conn.execute(insert_makan_stmt)
         self.conn.execute(tblstmt)
         self.conn.execute(itemidx)
         self.conn.execute(ownidx)
@@ -33,3 +38,10 @@ class DBHelper:
         stmt = "SELECT description FROM items WHERE owner = (?)"
         args = (owner,)
         return [x[0] for x in self.conn.execute(stmt, args)]
+
+
+    def add_vote(self, item_text, owner):
+        stmt = "INSERT INTO items (description, owner) VALUES (?, ?)"
+        args = (item_text, owner)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
